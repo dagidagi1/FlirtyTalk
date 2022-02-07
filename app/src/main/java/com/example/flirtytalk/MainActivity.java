052dc024ad6a9cd05fb7a12a91b890b3003b6515
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-02-07 18:17:51 +0200

    navgraph updated

--- app/src/main/java/com/example/flirtytalk/MainActivity.java
+++ app/src/main/java/com/example/flirtytalk/MainActivity.java
@@ -7,16 +7,31 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     NavController navController;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         NavHostFragment nav_host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_nav_controller);
         navController = nav_host.getNavController();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
+    }
 }
