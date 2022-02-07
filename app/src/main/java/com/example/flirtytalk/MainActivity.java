02c71012a5fa3dca18dda4eda7826673525f0c24
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-02-07 17:52:39 +0200

    navgraph updated

--- app/src/main/java/com/example/flirtytalk/MainActivity.java
+++ app/src/main/java/com/example/flirtytalk/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.flirtytalk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavHostFragment nav_host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_nav_controller);
+        navController = nav_host.getNavController();
     }
-    public void test(){
-        test();
-    }
+
 }
---
+++ app/src/main/java/com/example/flirtytalk/WelcomeFragment.java
@@ -0,0 +1,40 @@
+package com.example.flirtytalk;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class WelcomeFragment extends Fragment {
+
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navcontroller = Navigation.findNavController(view);
+        Button login_btn = view.findViewById(R.id.to_login_frame_btn);
+        Button register_btn = view.findViewById(R.id.to_register_frame_btn);
+        login_btn.setOnClickListener(p -> navcontroller.navigate(R.id.action_welcomeFragment_to_logInFragment));
+        register_btn.setOnClickListener(p -> navcontroller.navigate(R.id.action_welcomeFragment_to_registerFragment));
+    }
+}
