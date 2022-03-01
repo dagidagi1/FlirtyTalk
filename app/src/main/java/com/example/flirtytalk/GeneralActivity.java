f0900679bddc7d6eea3a5eff18140cb474538b20
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-03-01 02:37:19 +0200

    ux updated

--- app/src/main/java/com/example/flirtytalk/GeneralActivity.java
+++ app/src/main/java/com/example/flirtytalk/GeneralActivity.java
@@ -1,8 +1,11 @@
 package com.example.flirtytalk;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -10,14 +13,15 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.flirtytalk.Model.UsersModel;
 import com.example.flirtytalk.databinding.ActivityGeneralBinding;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
 public class GeneralActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityGeneralBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +35,28 @@ public class GeneralActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_general);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home).setOpenableLayout(drawer).build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_general);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        binding.appBarGeneral.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                navController.navigate(R.id.action_nav_home_to_postDetailsFragment);
-            }
-        });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (!super.onOptionsItemSelected(item)) {
+            Log.d("TAG", String.valueOf(item.getItemId()) + " = "+ String.valueOf(R.id.nav_log_out));
+            switch (item.getItemId()) {
+                case R.id.nav_log_out:
+                    UsersModel.instance.logout();
+                    finish();
+                    startActivity(new Intent(this, MainActivity.class));
+                    return true;
+                default:
+                    return NavigationUI.onNavDestinationSelected(item, navController);
+            }
+        }
+        return true;
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
